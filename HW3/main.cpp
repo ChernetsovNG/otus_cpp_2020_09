159b85db63cfd9ca90064cb76065aa60763add0a
ChernetsovNG <n.chernetsov86@gmail.com>
2020-11-22 15:29:00 +0300

    HW3: добавлены числовые типы и строки

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -1,4 +1,6 @@
+#include "print_ip.cpp"
 
 int main() {
+    print_ip(std::string{"127.0.0.1"});
     return 0;
 }
--- HW3/print_ip.cpp
+++ HW3/print_ip.cpp
@@ -1,5 +1,26 @@
+#include <iostream>
+#include <type_traits>
 
 template<typename T>
-void print_ip() {
+typename std::enable_if<std::is_integral<T>::value, void>::type
+print_ip(T ip) {
+    /*
+      Адрес может быть представлен в виде произвольного целочисленного типа. Представление не зависит от знака типа.
+      Выводить побайтово, начиная со старшего байта, с символом `.` (символ точки) в качестве разделителя
+     */
+    unsigned char b1, b2, b3, b4;
+    b1 = (ip & 0xFF);
+    b2 = ((ip >> 8) & 0xFF);
+    b3 = ((ip >> 16) & 0xFF);
+    b4 = ((ip >> 24) & 0xFF);
+    std::cout << (int) b4 << "." << (int) b3 << "." << (int) b2 << "." << (int) b1 << std::endl;
+}
 
+template<typename T>
+typename std::enable_if<std::is_same<T, std::string>::value, void>::type
+print_ip(T ip) {
+    /*
+      Адрес может быть представлен в виде строки. Выводится как есть
+     */
+    std::cout << ip << std::endl;
 }
