0a41757f70dc9a6ec831107f1efd43210dbd621f
ChernetsovNG <n.chernetsov86@gmail.com>
2020-11-22 16:39:18 +0300

    HW3: варианты с числами, строками, векторами и списками заработали. Остался tuple

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -1,6 +1,24 @@
+#include <vector>
+#include <tuple>
+
 #include "print_ip.cpp"
 
 int main() {
-    print_ip(std::string{"127.0.0.1"});
+    long integer_ip = 3221226219;
+    print_ip(integer_ip);
+
+    std::string string{"127.0.0.1"};
+    print_ip(string);
+
+    std::vector<int> vector{225, 121, 15, 8};
+    print_ip(vector);
+
+    std::list<int> list{192, 15, 0, 4};
+    print_ip(list);
+
+    std::tuple<int> t2{10};
+
+    //print_ip(t2);
+
     return 0;
 }
--- HW3/print_ip.cpp
+++ HW3/print_ip.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <type_traits>
+#include <vector>
+#include <list>
+#include <tuple>
 
+/*
+  Адрес может быть представлен в виде произвольного целочисленного типа. Представление не зависит от знака типа.
+  Выводить побайтово, начиная со старшего байта, с символом `.` (символ точки) в качестве разделителя
+ */
 template<typename T>
-typename std::enable_if<std::is_integral<T>::value, void>::type
+typename std::enable_if_t<std::is_integral_v<T>, void>
 print_ip(T ip) {
-    /*
-      Адрес может быть представлен в виде произвольного целочисленного типа. Представление не зависит от знака типа.
-      Выводить побайтово, начиная со старшего байта, с символом `.` (символ точки) в качестве разделителя
-     */
     unsigned char b1, b2, b3, b4;
     b1 = (ip & 0xFF);
     b2 = ((ip >> 8) & 0xFF);
@@ -16,11 +19,45 @@ print_ip(T ip) {
     std::cout << (int) b4 << "." << (int) b3 << "." << (int) b2 << "." << (int) b1 << std::endl;
 }
 
+/*
+  Адрес может быть представлен в виде строки. Выводится как есть
+ */
 template<typename T>
-typename std::enable_if<std::is_same<T, std::string>::value, void>::type
+typename std::enable_if_t<std::is_same_v<T, std::string>, void>
 print_ip(T ip) {
-    /*
-      Адрес может быть представлен в виде строки. Выводится как есть
-     */
     std::cout << ip << std::endl;
 }
+
+/*
+  Адрес может быть представлен в виде контейнеров `std::list`, `std::vector`. Выводится содержимое контейнера
+  поэлементно и разделяется `.` (одним символом точка)
+ */
+template<typename T>
+void print_ip(std::vector<T> ip) {
+    for (int i = 0; i < 3; ++i) {
+        std::cout << ip[i] << ".";
+    }
+    std::cout << ip[3] << std::endl;
+}
+
+template<typename T>
+void print_ip(std::list<T> ip) {
+    auto it = std::begin(ip);
+    if (it != std::end(ip))
+        do {
+            std::cout << *it;
+            if (++it == std::end(ip)) break;
+            std::cout << ".";
+        } while (true);
+    std::cout << std::endl;
+}
+
+/*
+  Опционально адрес может быть представлен в виде `std::tuple` при условии, что все типы одинаковы. Выводится
+  содержимое поэлементно и разделяется `.` (одним символом точка)
+ */
+/*template<typename T, typename... U>
+typename std::enable_if_t<std::is_same_v<T, std::tuple<U...>> && std::is_integral_v<U>, void>
+print_ip(T ip) {
+    std::cout << "tuple" << std::endl;
+}*/
