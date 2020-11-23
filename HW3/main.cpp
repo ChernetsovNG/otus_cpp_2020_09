cee35ee5b08f9f6372ca78a8ef55da3fbb5fe116
NChernetsov <n.chernetsov86@gmail.com>
2020-11-23 11:14:42 +0300

    HW3: предложен вариант печати tuple, но без проверки типов

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -4,7 +4,7 @@
 #include "print_ip.cpp"
 
 int main() {
-    long integer_ip = 3221226219;
+    auto integer_ip = 3221226219;
     print_ip(integer_ip);
 
     std::string string{"127.0.0.1"};
@@ -16,9 +16,8 @@ int main() {
     std::list<int> list{192, 15, 0, 4};
     print_ip(list);
 
-    std::tuple<int> t2{10};
-
-    //print_ip(t2);
+    std::tuple<int, int, int, int> t2{117, 218, 33, 10};
+    print_ip(t2);
 
     return 0;
 }
--- HW3/print_ip.cpp
+++ HW3/print_ip.cpp
@@ -4,13 +4,15 @@
 #include <list>
 #include <tuple>
 
+#define UNUSED(x) (void)(x)
+
 /*
-  Адрес может быть представлен в виде произвольного целочисленного типа. Представление не зависит от знака типа.
+  1. Адрес может быть представлен в виде произвольного целочисленного типа. Представление не зависит от знака типа.
   Выводить побайтово, начиная со старшего байта, с символом `.` (символ точки) в качестве разделителя
  */
 template<typename T>
 typename std::enable_if_t<std::is_integral_v<T>, void>
-print_ip(T ip) {
+print_ip(T &ip) {
     unsigned char b1, b2, b3, b4;
     b1 = (ip & 0xFF);
     b2 = ((ip >> 8) & 0xFF);
@@ -20,20 +22,20 @@ print_ip(T ip) {
 }
 
 /*
-  Адрес может быть представлен в виде строки. Выводится как есть
+  2. Адрес может быть представлен в виде строки. Выводится как есть
  */
 template<typename T>
 typename std::enable_if_t<std::is_same_v<T, std::string>, void>
-print_ip(T ip) {
+print_ip(T &ip) {
     std::cout << ip << std::endl;
 }
 
 /*
-  Адрес может быть представлен в виде контейнеров `std::list`, `std::vector`. Выводится содержимое контейнера
+  3. Адрес может быть представлен в виде контейнеров `std::list`, `std::vector`. Выводится содержимое контейнера
   поэлементно и разделяется `.` (одним символом точка)
  */
 template<typename T>
-void print_ip(std::vector<T> ip) {
+void print_ip(std::vector<T> &ip) {
     for (int i = 0; i < 3; ++i) {
         std::cout << ip[i] << ".";
     }
@@ -41,7 +43,7 @@ void print_ip(std::vector<T> ip) {
 }
 
 template<typename T>
-void print_ip(std::list<T> ip) {
+void print_ip(std::list<T> &ip) {
     auto it = std::begin(ip);
     if (it != std::end(ip))
         do {
@@ -53,11 +55,27 @@ void print_ip(std::list<T> ip) {
 }
 
 /*
-  Опционально адрес может быть представлен в виде `std::tuple` при условии, что все типы одинаковы. Выводится
+  4. Опционально адрес может быть представлен в виде `std::tuple` при условии, что все типы одинаковы. Выводится
   содержимое поэлементно и разделяется `.` (одним символом точка)
+
+  Предложенный вариант функции печати не проверяет одинаковость типов tuple
  */
-/*template<typename T, typename... U>
-typename std::enable_if_t<std::is_same_v<T, std::tuple<U...>> && std::is_integral_v<U>, void>
-print_ip(T ip) {
-    std::cout << "tuple" << std::endl;
-}*/
+template<std::size_t I = 0, typename... T>
+typename std::enable_if_t<I == sizeof...(T), void>
+print_ip(std::tuple<T...> &t) {
+    UNUSED(I);
+    UNUSED(t);
+}
+
+template<std::size_t I = 0, typename... T>
+typename std::enable_if_t<I == sizeof...(T) - 1, void>
+print_ip(std::tuple<T...> &t) {
+    std::cout << std::get<I>(t);
+}
+
+template<std::size_t I = 0, typename... T>
+typename std::enable_if_t<I < sizeof...(T) - 1, void>
+print_ip(std::tuple<T...> &t) {
+    std::cout << std::get<I>(t) << ".";
+    print_ip<I + 1, T...>(t);
+}
